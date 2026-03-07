e8b5598aee0229824b15a3ee392d182b199f30ba
Terrance Gateri <tkgateri@gmail.com>
2026-03-07 17:01:20 +0300

    javascript commit

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+
+<template>
+    <v-container max width ="100%">
+        <v-row>
+            <v-carousel>
+                <v-carousel-item src="landscape1.jpg"></v-carousel-item>
+                <v-carousel-item src=""></v-carousel-item>
+                <v-carousel-item src=""></v-carousel-item>
+            </v-carousel>
+        </v-row>
+    </v-container>
+
+    <!-- About Us -->
+     <v-container color="">
+        <v-row >
+            <v-col md="6">
+                <div class="text-display-medium">Welcome to MacFit Gym</div>
+                <div class="text-title-large">Paragraph 1</div>
+                <div class="text-title-large">Paragraph 2</div>
+                <div class="text-title-large">Paragraph 3</div>
+                <v-btn>LEARN MORE</v-btn>
+            </v-col>
+            <v-col md="6">
+                <v-img src=""></v-img>
+            </v-col>
+        </v-row>
+     </v-container>
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+<template>
+    <v-app-bar color="#A51AA7">
+        <v-app-bar-title>MacFit Gym</v-app-bar-title>
+        <v-btn to="/homepage">Home</v-btn>
+        <v-btn to="/login">Login</v-btn>
+        <v-btn to="/signup">Sign Up</v-btn>
+        <v-btn to="/profile">Profile</v-btn>
+        <v-btn to="/bundles">Bundles</v-btn>
+        <v-btn to="/gym_locations">Gym Locations</v-btn>
+        <v-btn to="/admin">Admin</v-btn>
+    </v-app-bar>
+</template>
