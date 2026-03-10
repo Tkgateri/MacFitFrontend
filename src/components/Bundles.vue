4c377a5dbf5ae256c2f628b388eef3020406ad76
Tkgateri <tkgateri@gmail>
2026-03-10 02:47:30 -0700

    login and sign up updates

--- src/components/Bundles.vue
+++ src/components/Bundles.vue
@@ -3,35 +3,35 @@
 </script>
 
 <template>
-    <v-container style="background-color:#CFD0D6">
+    <v-container style="background-color:#CFD0D6" class="mt-12">
         <v-row>
             <div class="text-display-medium mb-12">Bundles and Pricing</div>
         </v-row>
         <v-row>
             <v-col md="3">
                 <v-card class="text-center">
-                    <v-icon color="#7B3F8F" icon="mdi-clock-outline" size="large"></v-icon>
+                    <v-icon color="#7B3F8F" icon="mdi-clock-outline" size="large" class="mt-8"></v-icon>
                     <v-card-title color="#7B3F8F">Daily Pass</v-card-title>
                     <v-card-text>Kshs. 500</v-card-text>
                 </v-card>
             </v-col>
             <v-col md="3">
                 <v-card class="text-center">
-                    <v-icon color="#7B3F8F" icon="mdi-calendar-account-outline" size="large"></v-icon>
+                    <v-icon color="#7B3F8F" icon="mdi-calendar-account-outline" size="large" class="mt-8"></v-icon>
                     <v-card-title color="#7B3F8F">1 month</v-card-title>
                     <v-card-text>Kshs. 15,000</v-card-text>
                 </v-card>
             </v-col>
             <v-col md="3">
                 <v-card class="text-center">
-                    <v-icon color="#7B3F8F" icon="mdi-numeric-3-circle" size="large"></v-icon>
+                    <v-icon color="#7B3F8F" icon="mdi-numeric-3-circle" size="large" class="mt-8"></v-icon>
                     <v-card-title color="#7B3F8F">3 Months</v-card-title>
                     <v-card-text>Kshs. 45,000</v-card-text>
                 </v-card>
             </v-col>
             <v-col md="3">
                 <v-card class="text-center">
-                    <v-icon color="#7B3F8F" icon="mdi-numeric-6-box-multiple-outline" size="large"></v-icon>
+                    <v-icon color="#7B3F8F" icon="mdi-numeric-6-box-multiple-outline" size="large" class="mt-8"></v-icon>
                     <v-card-title color="#7B3F8F">6 Months</v-card-title>
                     <v-card-text>Kshs. 54,000</v-card-text>
                 </v-card>
@@ -40,11 +40,61 @@
               <v-row>
             <v-col md="12">
                 <v-card class="text-center">
-                    <v-icon color="#7B3F8F" icon="mdi-timer-sand-full" size="large"></v-icon>
+                    <v-icon color="#7B3F8F" icon="mdi-timer-sand-full" size="large" class="mt-8"></v-icon>
                     <v-card-title color="#3A4B68">12 Months</v-card-title>
                     <v-card-text>Kshs. 70,000</v-card-text>
                 </v-card>
             </v-col>
         </v-row>
     </v-container>
+    <!-- What is included? -->
+     <v-container style="background-color:#CFD0D6" class="mt-12">
+        <v-row>
+            <div class="text-display-medium mb-12">What is included?</div>
+        </v-row>
+        <v-row>
+            <v-col md="3">
+                <v-card class="text-center">
+                    <v-icon color="#7B3F8F" icon="mdi-food-apple-outline" size="large" class="mt-8"></v-icon>
+                    <v-card-title color="#7B3F8F">Personalized Diet Plans</v-card-title>
+                   
+                </v-card>
+            </v-col>
+            <v-col md="3">
+                <v-card class="text-center">
+                    <v-icon color="#7B3F8F" icon="mdi-weight-lifter" size="large" class="mt-8"></v-icon>
+                    <v-card-title color="#7B3F8F">Gym Membership Card</v-card-title>
+                    
+                </v-card>
+            </v-col>
+            <v-col md="3">
+                <v-card class="text-center">
+                    <v-icon color="#7B3F8F" icon="mdi-hours-24" size="large" class="mt-8"></v-icon>
+                    <v-card-title color="#7B3F8F">24/7 Cosultation</v-card-title>
+                    
+                </v-card>
+            </v-col>
+            <v-col md="3">
+                <v-card class="text-center">
+                    <v-icon color="#7B3F8F" icon="mdi-swim" size="large" class="mt-8"></v-icon>
+                    <v-card-title color="#7B3F8F">Access to a pool</v-card-title>
+                </v-card>
+            </v-col>           
+        </v-row>
+        </v-container>
+        <v-container style="background-color:#CFD0D6" class="mt-12">
+            <v-row>
+            <div class="text-display-medium mb-12">How to Join</div>
+            </v-row>
+        <v-row>
+            <v-col>
+                <v-list>
+                    <v-list-item>1. Register for membership through our website. </v-list-item>
+                    <v-list-item>2. Select a bundle that best suites you and make payment through Mpesa payblill number 555000.</v-list-item>
+                    <v-list-item>3. Forward the payment confirmation message to our email address "macfitgym@gmail.com"</v-list-item>
+                    <v-list-item>4. Finally, make an appearance at our gym and begin your transformation journey! </v-list-item>
+                </v-list>
+            </v-col>
+        </v-row>
+     </v-container>
 </template>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,7 +1,139 @@
 <script setup>
+import { ref } from 'vue'
+
+  const rules = {
+    required: value => !!value || 'Required.',
+    min: v => v.length >= 8 || 'Min 8 characters',
+    emailMatch: () => (`The email and password you entered don't match`),
+  }
+
+  const show1 = ref(false)
+  const show2 = ref(true)
+  const password = ref(null)
+  const confirmPassword = ref(null)
+  const show1confirm = ref(false)
 
 </script>
-
 <template>
-    <h1>Sign Up</h1>
+    <v-container width="50%" class="text-center mt-12" color="#CFD0D6">
+        <v-row>
+            <v-col>
+                <v-form>
+                    <v-row>
+                        <v-col md="12">
+                            <v-icon size="large">mdi-dumbbell</v-icon>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col>
+                            <div class="text-display-small font-weight-medium">Sign in to MacFit Gym</div>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col md="6">
+                            <div class="text-display-small font-weight-medium text-right">First Name</div>
+                        </v-col>
+                        <v-col md="6">
+                            <v-text-field variant="outlined"></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col md="6">
+                            <div class="text-display-small font-weight-medium text-right">Last Name</div>
+                        </v-col>
+                        <v-col md="6">
+                            <v-text-field variant="outlined"></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col md="6">
+                            <div class="text-display-small font-weight-medium text-right">Email</div>
+                        </v-col>
+                        <v-col md="6">
+                            <v-text-field variant="outlined"></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col md="6">
+                            <div class="text-display-small font-weight-medium text-right">Phone</div>
+                        </v-col>
+                        <v-col md="6">
+                            <v-text-field variant="outlined" type="number"></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col md="6">
+                            <div class="text-display-small font-weight-medium text-right">Gender</div>
+                        </v-col>
+                        <v-col md="6">
+                            <v-radio-group inline>
+                                <v-radio label="Male" value="Male"></v-radio>
+                                <v-radio label="Female" value="Female"></v-radio>
+                            </v-radio-group>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col md="6">
+                            <div class="text-display-small font-weight-medium text-right">Date of Birth</div>
+                        </v-col>
+                        <v-col md="6">
+                            <v-date-input variant="outline"></v-date-input>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col md="6">
+                            <div class="text-display-small font-weight-medium text-right">Gym Location</div>
+                        </v-col>
+                        <v-col md="6">
+                            <v-select
+                                label="Select"
+                                :items="['CBD', 'Madaraka', 'Westlands', 'Buruburu',]">
+
+                                </v-select>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col md="6">
+                            <div class="text-display-small font-weight-medium text-right">Password</div>
+                        </v-col>
+                        <v-col md="6">
+                            <v-text-field
+                            variant="outlined"
+                            v-model="password"
+                            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                            :rules="[rules.required, rules.min]"
+                            :type="show1 ? 'text' : 'password'"
+                            name="input-10-1"
+                            @click:append="show1 = !show1"></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col md="6">
+                            <div class="text-display-small font-weight-medium text-right">Confirm Password</div>
+                        </v-col>
+                        <v-col md="6">
+                            <v-text-field
+                            variant="outlined"
+                            v-model="confirmPassword"
+                            :append-icon="show1confirm ? 'mdi-eye' : 'mdi-eye-off'"
+                            :rules="[rules.required, rules.min]"
+                            :type="show1 ? 'text' : 'password'"
+                            name="input-10-1"
+                            @click:append="show1 = !show1"></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col md="12">
+                            <v-btn color="#CFD0D6" variant="elevated" width="25%">Log in</v-btn>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col md="12">
+                            <div>New to MacFit Gym? Create an account</div>
+                        </v-col>
+                    </v-row>
+                </v-form>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
